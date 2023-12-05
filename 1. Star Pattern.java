d08fdc5e8d9b787fcb8c0e4c4d67deb2825a0ff6
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-12-05 13:48:21 -0500

    Update 1. Star Pattern.java

--- 1. Star Pattern.java
+++ 1. Star Pattern.java
@@ -7,7 +7,7 @@ class My_Application {
  
     public static void main(String[] args) {
     
-   for (int i = 0; i < 5; i++) { //will keep repeating this loop at long as 'i' is less than 5
+   for (int i = 0; i < 5; i++) { //will keep repeating this loop at long as 'i' is less than 5, 'i' is getting increased by 1 every time the loop executes
        System.out.println("********"); //this statment will be printed every time the loop executes.
    }
     
