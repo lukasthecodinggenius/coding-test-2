90a8a88f1625c3e07bea8b7022cd7f23460b6b34
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2023-12-05 13:40:13 -0500

    Create 1. Star Pattern.java

---
+++ 1. Star Pattern.java
@@ -0,0 +1,25 @@
+
+package my_application;
+import java.util.Scanner;
+
+class My_Application {
+        
+ 
+    public static void main(String[] args) {
+       
+    Scanner scan = new Scanner(System.in);
+    
+   for (int i = 0; i < 5; i++) {
+       System.out.println("********");
+   }
+    
+            
+    }
+        
+  
+     
+     
+              } 
+    
+
+
